1ae35fece07a625ec06698e55af5e1456b262a9b
Nícolas Saraiva <nicolasrsaraiva@gmail.com>
2023-07-19 08:53:36 -0300

    Initial commit

---
+++ src/main/java/com/study/pokemonapispring/PokemonApiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.study.pokemonapispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonApiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonApiSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/pokemonapispring/PokemonApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.pokemonapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
